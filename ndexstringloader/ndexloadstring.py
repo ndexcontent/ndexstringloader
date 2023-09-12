9deb88e02c8b60c9d8a7d04013c06e91b293018d
rpillich <rpillich@ucsd.edu>
2023-09-12 15:06:01 -0700

    Updated network description
    Description now reflects the changes in the visual style used.

--- ndexstringloader/ndexloadstring.py
+++ ndexstringloader/ndexloadstring.py
@@ -727,8 +727,8 @@ class NDExSTRINGLoader(object):
 
         net_attributes['description'] = '<br>This network contains high confidence (score >= ' \
                     + str(cutoffscore) + ') human protein links with combined scores. ' \
-                    + 'Edge color was mapped to the combined score value using a gradient from light grey ' \
-                    + '(low Score) to black (high Score).'
+                    + 'Edge color was mapped to the combined score value using a yellow-green-purple gradient for score values >0.7.' \
+                    + 'Edges with score values <0.7 are shown grey. '
 
         rights = 'Attribution 4.0 International (CC BY 4.0)'
         net_attributes['rights'] = self._get_property_from_summary('rights', summary, rights)
